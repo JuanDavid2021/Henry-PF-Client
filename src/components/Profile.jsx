d34dacb28e9f4ee39aa8295b5837c08da6c432a0
JuanDavid2021 <davidyk_15@hotmail.com>
2022-03-15 14:45:40 -0500

    update Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,13 +4,12 @@ import { useAuth0 } from '@auth0/auth0-react'
 const Profile = () => {
 
     const {user}= useAuth0()
-
+    
   return (
     <div>
       <div>
           <img src={user.picture} alt={user.name}/>
           <h2>{user.name}</h2>
-          <p>{user.email}</p>
       </div>
     </div>
   )
