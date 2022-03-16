54995afadad1df7474a31c6ad5aa20c203df4e7a
valentino spada <valenspada@hotmail.com>
2022-03-16 17:37:20 -0300

    rutas y shop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import Shop from './components/Shop';
+
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Login/>
-      <Profile/>
+      <NavBar />
+      <Routes>
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/shop' element={<Shop />} />
+        <Route exact path='/profile/:id' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { RiShoppingCartLine } from 'react-icons/ri'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
 
@@ -12,18 +13,18 @@ function NavBar() {
 
     return (
             <nav className="navbar navbar-expand-lg flex-column bg-dark">
-                <div class="container-fluid">
-                    <a className="text-light text-decoration-none fs-4 mx-3 navbar-brand" href="#">Beef Shop</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon btn-outline-light" type="button"><GiHamburgerMenu /></span>
+                <div className="container-fluid">
+                    <Link className="text-light text-decoration-none fs-4 mx-3 navbar-brand" to="/">Beef Shop</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon btn-outline-light" type="button"><GiHamburgerMenu /></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent" >
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="text-light text-decoration-none fw-light fs-5 mx-3 nav-link" data-bs-toggle="collapse" href="#">Home</a>
+                                <Link className="text-light text-decoration-none fw-light fs-5 mx-3 nav-link" to='/shop'>Shop</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="text-light text-decoration-none fw-light fs-5 mx-3 nav-link" href="#">About</a>
+                                <Link className="text-light text-decoration-none fw-light fs-5 mx-3 nav-link" to="/about">About</Link>
                             </li>
                         </ul>
                         {
