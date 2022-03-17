601d58da153c7c64e3872eb54a15096f2099cad2
valentino spada <valenspada@hotmail.com>
2022-03-17 12:12:37 -0300

    shop nav searchbar logo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
+
+    const [input, setInput] = useState("")
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setInput(e.target.value)
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setInput('')
+    }
+
+
+    return (
+        <div style={{ width: "20%" }}>
+            <div className="input-group mb-3">
+                <input type="text" onChange={handleChange} value={input} className="form-control" placeholder="Especial Search" aria-label="Especial Search" aria-describedby="button-addon2" />
+                <button className="btn btn-dark" onClick={handleClick} type="button" id="button-addon2">Search</button>
+            </div>
+        </div>
+    )
 }
 
 export default SearchBar
--- src/dataSimulate.js
+++ src/dataSimulate.js
@@ -48,7 +48,89 @@ const p1 = JSON.parse(`{
       "Some quick example text to build on the card title and make up the bulk of the card's content."
   }`)
 
+  const p5 = JSON.parse(`{
+    "id":5,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
 
-const arrProductos = [p1,p2,p3,p4];
+  const p6 = JSON.parse(`{
+    "id":6,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
+
+
+  const p7 = JSON.parse(`{
+    "id":7,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
+
+
+  const p8 = JSON.parse(`{
+    "id":8,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
+
+
+  const p9 = JSON.parse(`{
+    "id":9,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
+
+
+  const p10 = JSON.parse(`{
+    "id":10,
+    "nombre": "bola de lomo",
+    "precio": 850,
+    "fotos":[
+      "https://static.wixstatic.com/media/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.png/v1/fill/w_278,h_278,al_c,q_85,usm_0.66_1.00_0.01/91fc24_8b6b6cd3d7ad49ed9695daa84257494a~mv2.webp", 
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-1.jpg",
+      "http://azim.commonsupport.com/Carneshop/assets/images/resource/shop/shop-2.jpg"
+    ],
+    "presentacion":
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }`)
+
+
+const arrProductos = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10];
 
 export default arrProductos
