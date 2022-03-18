0e6991b906ce4e3b1ed68bcab2519eab718391d2
Franco <fegll87@gmail.com>
2022-03-17 23:13:11 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,36 @@
 import React, { useState } from "react";
-import { Button, Card as CardBootstrap, Carousel, Col, Container, } from "react-bootstrap";
-import { RiShoppingCartLine } from 'react-icons/ri';
-/* import { useDispatch } from "react-redux"; */
-/* import { Link } from "react-router-dom"; */
+import { Button, Card as CardBootstrap, Col } from "react-bootstrap";
 import DetailProductModal from './DetailProductModal';
 
-function Card({ nombre, presentacion, precio, arrFotos }) {
-  const Nombre = nombre?.[0].toUpperCase() + nombre.slice(1).toLowerCase();
-  /* const dispatch = useDispatch(); */
+function Card({id, nombre, stock, presentacion, precio, arrFotos }) {
+  const nombreCap = nombre?.[0].toUpperCase() + nombre?.slice(1).toLowerCase();
+  let randomPic = Math.floor(Math.random() * arrFotos?.length)
+
   const [show, setShow] = useState(false);
+
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
-
+  
   return (
-    <Col xs={12} sm={12} md={6} lg={4} xl={3}>
-      <CardBootstrap style={{ alignItems: "center", height: "fit-content" }}>
-        <Carousel fade variant="dark" style={{ width: "10em" }}>
-          {arrFotos?.map((el) => (
-            <Carousel.Item style={{ width: "10em" }} key={el}>
-              <img
-                className="d-block w-100"
-                src={el}
-                alt={el}
-                style={{ height: "10em" }}
-              />
-            </Carousel.Item>
-          ))}
-        </Carousel>
+    <Col xs={12} sm={12} md={6} lg={4} >
+      <CardBootstrap style={{ height: "100%"}}>
+        <Col style={{ display:"flex", justifyContent:"center" }}>
+          <CardBootstrap.Img variant="top" style={{ height: "12em", width: "12em" , alignItems: "center" }} src={arrFotos && arrFotos[randomPic]} />
+        </Col>
         <CardBootstrap.Body>
-          <CardBootstrap.Title>{Nombre}</CardBootstrap.Title>
-          <CardBootstrap.Subtitle>$ {precio}</CardBootstrap.Subtitle>
-          <CardBootstrap.Text>{presentacion}</CardBootstrap.Text>
-          <Container>
-              <Button variant="dark" size="sm" onClick={handleShow}>
-                Detalle
-              </Button>{" "}
-            <Button variant="dark" size="sm" /* onClick={()=>{dispatch()}} */>
-              Carrito <RiShoppingCartLine />
-            </Button>
-          </Container>
+          <CardBootstrap.Title>{nombreCap}</CardBootstrap.Title>
+          <CardBootstrap.Subtitle>$ {precio}/kg</CardBootstrap.Subtitle>
+          <Col>
+            <CardBootstrap.Text>{presentacion}</CardBootstrap.Text>
+          </Col>
         </CardBootstrap.Body>
+        <Col>
+            <Button style={{ width: "100%" }} variant="dark" onClick={handleShow}>
+              Detalle
+            </Button>
+        </Col>
       </CardBootstrap>
-      <DetailProductModal Nombre={Nombre} precio={precio} presentacion={presentacion} arrFotos={arrFotos} show={show} handleClose={handleClose}/>
+      <DetailProductModal id={id} nombreCap={nombreCap} stock={stock} precio={precio} presentacion={presentacion} arrFotos={arrFotos} show={show} handleClose={handleClose}/>
     </Col>
   );
 }
