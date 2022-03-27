8064975760cea359480314fa9ba3f492bd9c83c3
Alejo-Nino95 <alejo_120295_hotmail.com>
2022-03-27 01:42:59 -0500

    ...

--- src/components/Pedidos.jsx
+++ src/components/Pedidos.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RiDeleteBin5Fill, RiArrowRightSLine, RiArrowLeftSLine } from 'react-icons/ri';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-import { deleteCartItem, setCartItem, getPedidos,apiGetAllUsers } from '../actions';
+import { deleteCartItem, setCartItem, getPedidos, apiGetAllUsers } from '../actions';
 import { Modal, Button } from 'react-bootstrap';
 
 function Pedidos() {
@@ -16,7 +16,54 @@ function Pedidos() {
     }, [dispatch])
 
     return (
-        <p>Hola</p>
+        <div className="table-responsive ">
+            <table className="table text-nowrap ">
+                <thead  >
+                    <tr className="text-sm ">
+                        <th className="border-gray-300 border-top py-3">Fecha Pedido</th>
+                        <th className="border-gray-300 border-top py-3">Fecha Despacho</th>
+                        <th className="border-gray-300 border-top py-3">Usuario</th>
+                        <th className="border-gray-300 border-top py-3"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        pedidos.length > 0 ?
+                            /* Mapeo de las filas de la tabla con los items del carrito */
+                            pedidos.map((p) => {
+                                return (
+                                    <tr className="lead text-center fs-3 fw-normal mt-3">
+                                        <td>
+                                            {p.f_pedido}
+                                        </td>
+                                        <td>
+                                            {p.f_requerida}
+                                        </td>
+                                        <td>
+                                            {p.UsuarioCorreo}
+                                        </td>
+                                        <td>
+                                            <button>Despachar</button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            )
+                            : <tr className="lead text-center fs-3 fw-normal mt-3">
+                                <td>
+                                    No tiene productos en el carrito
+                                </td>
+                            </tr>
+                    }
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th className="py-3 border-0" colSpan="5"> <span className="h4 text-gray-700 mb-0">Total</span></th>
+                        <th className="py-3 border-0 text-end" colSpan="2"> <span className="h4 text-gray-700 mb-0">$ {suma}</span></th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     )
 }
 
