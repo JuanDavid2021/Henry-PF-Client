f26af27962dcedfef73ccd7c461cf6a853db1be2
Franco <fegll87@gmail.com>
2022-04-02 14:03:06 -0300

    button width and style fixes

--- src/components/Pedidos.jsx
+++ src/components/Pedidos.jsx
@@ -23,19 +23,16 @@ function Pedidos() {
 
     return (
         <div className='container'>
-            <Row className="border-top border-2 border-dark">
+            <Row >
                 <SearchBar filtro={true}/>
             </Row>
-            <div className="table-responsive ">
-                <table className="table text-nowrap ">
+            <div className="table-responsive">
+                <table className="table text-nowrap">
                     <thead  >
-                        <tr className="text-sm ">
+                        <tr className="text-sm">
                             <th className="border-gray-300 border-top py-3">Fecha Pedido</th>
                             <th className="border-gray-300 border-top py-3">Fecha Despacho</th>
                             <th className="border-gray-300 border-top py-3">Usuario</th>
-                            <th className="border-gray-300 border-top py-3"></th>
-                            <th className="border-gray-300 border-top py-3"></th>
-                            <th className="border-gray-300 border-top py-3"></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -44,7 +41,7 @@ function Pedidos() {
                                 /* Mapeo de las filas de la tabla con los items del carrito */
                                 pedidos.map((p) => {
                                     return (
-                                        <tr className="lead text-center fs-4 fw-normal mt-3">
+                                        <tr className="text-sm">
                                             <td>
                                                 {p.f_pedido.substring(0, 10)}
                                             </td>
@@ -55,7 +52,7 @@ function Pedidos() {
                                                 {p.UsuarioCorreo}
                                             </td>
                                             <td>
-                                                <button className="btn btn-info text-light text-decoration-none fs-6 mx-2" onClick={() => handleDetailPedido(p.id)}>Detalles</button>
+                                                <button className="btn btn-info text-light text-decoration-none mx-2" onClick={() => handleDetailPedido(p.id)}>Detalles</button>
                                             </td>
                                         </tr>
                                     )
@@ -70,8 +67,8 @@ function Pedidos() {
                     </tbody>
                     <tfoot>
                         <tr>
-                            <th className="py-3 border-0" colSpan="5"> <span className="h5 text-gray-700 mb-0">Total</span></th>
-                            <th className="py-3 border-0 text-end" colSpan="2"> <span className="h5 text-gray-700 mb-0">{pedidos.length}</span></th>
+                            <th className="py-3 border-0" colSpan="3"> <span className="h5 text-gray-700 mb-0">Total</span></th>
+                            <th className="py-3 border-0 text-center" colSpan="1"> <span className="h5 text-gray-700 mb-0">{pedidos.length}</span></th>
                         </tr>
                     </tfoot>
                 </table>
