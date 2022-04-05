b3fc42caaac0da4e0e343b5ea0c473c94dc4df39
valentino spada <valenspada@hotmail.com>
2022-04-05 15:06:10 -0300

    review

--- src/components/DetailCompra.jsx
+++ src/components/DetailCompra.jsx
@@ -10,7 +10,8 @@ import {
 import { RiArrowGoBackFill } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-import { getPedidos } from "../actions";
+import { getPedidos, addReview } from "../actions";
+import ReviewModal from "./ReviewModal";
 
 function DetailCompra() {
 
@@ -28,6 +29,14 @@ function DetailCompra() {
         dispatch(getPedidos(currenuser, pedidoId));
     }, [dispatch])
 
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => setShow(true);
+    const handleClosed = () => setShow(false);
+
+    console.log(pedido.ItemsPedidos)
+
+
     const handleClose = () => {
         navigate("/profile");
     };
@@ -38,110 +47,111 @@ function DetailCompra() {
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
         </>) : (
-        <div className="container">
-            <div className="row justify-content-center displaySm">
+            <div className="container">
+                <div className="row justify-content-center displaySm">
 
-                <div className="col col-sm-9" style={{ marginBottom: "40px" }}>
-                    <div style={{ width: "100%", marginTop: "30px", display: "flex", justifyContent: "center" }}>
-                        <Button style={{ width: "120px" }} variant="dark" onClick={handleClose}>
-                            Mi perfil <RiArrowGoBackFill />
-                        </Button>
+                    <div className="col col-sm-9" style={{ marginBottom: "40px" }}>
+                        <div style={{ width: "100%", marginTop: "30px", display: "flex", justifyContent: "center" }}>
+                            <Button style={{ width: "120px" }} variant="dark" onClick={handleClose}>
+                                Mi perfil <RiArrowGoBackFill />
+                            </Button>
+                        </div>
+                        <Card style={{ marginBottom: "30px", marginTop: "30px" }}>
+                            <Card.Header>
+                                {pedido.ItemsPedidos.map(p => p.nombre).join(" || ")}
+                            </Card.Header>
+                        </Card>
+                        <Card>
+                            <Card.Body>
+                                <Row>
+                                    <Col
+                                        lg={6}
+                                        xl={6}
+                                        style={{ display: "flex", flexDirection: "column" }}
+                                    >
+                                        <Row style={{ display: "flex", justifyContent: "center" }}>
+                                            {pedido.status === "Aprobada" ?
+                                                <b className="text-success mb-3">{pedido.status}</b>
+                                                :
+                                                <p>{pedido.status}</p>
+                                            }
+
+                                        </Row>
+                                        <Row style={{ display: "flex", justifyContent: "center" }}>
+                                            <p className="fs-4">Fecha de entrega {pedido.f_requerida.substring(0, 10)}</p>
+                                        </Row>
+                                        <Row style={{ display: "flex", justifyContent: "center" }}>
+                                            <p className="fs-6">La compra fue realizada el {pedido.f_pedido.substring(0, 10)}</p>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                        <Row className="mt-3">
+                            <p><b>Productos:</b></p>
+                        </Row>
+                        {
+                            pedido.ItemsPedidos.map((i, el) => (
+                                <Card className="mb-3">
+                                    <Card.Header as="h5">{i.nombre}</Card.Header>
+                                    <Card.Body>
+                                        <Row className="row row-wrap">
+                                            <Col className="col col-lg-10">
+                                                <Card.Title>Peso: {i.peso} kg</Card.Title>
+                                                <Card.Text>
+                                                    <p>Cantidad: {i.cantidad}</p>
+                                                    <p>Tipo de corte: {i.tipo_corte}</p>
+                                                </Card.Text>
+                                            </Col>
+                                            <Col className="col col-lg-2 col-md-2 position-relative">
+                                                <img src={i.Producto.fotos[0]} alt={i.nombre} className="rounded-circle img-fluid position-absolute" style={{ width: "15vh", top: "1rem", right: "1.25rem" }}></img>
+                                            </Col>
+
+                                        </Row>
+                                        <Button onClick={handleShow} variant="primary">Opinar sobre el producto</Button>
+                                    </Card.Body>
+                                    <ReviewModal nombreCap={i.nombre} id={i.ProductoId} toDispatch={addReview} status={pedido.status} show={show} handleClose={handleClosed} />
+                                </Card>
+                            ))
+                        }
                     </div>
-                    <Card style={{ marginBottom: "30px", marginTop: "30px" }}>
-                        <Card.Header>
-                            {pedido.ItemsPedidos.map(p => p.nombre).join(" || ")}
-                        </Card.Header>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                            <Row>
-                                <Col
-                                    lg={6}
-                                    xl={6}
-                                    style={{ display: "flex", flexDirection: "column" }}
-                                >
-                                    <Row style={{ display: "flex", justifyContent: "center" }}>
-                                        {pedido.status === "Aprobada" ?
-                                            <b className="text-success mb-3">{pedido.status}</b>
-                                            :
-                                            <p>{pedido.status}</p>
-                                        }
+                    <div className="col col-sm-2 sticky-top resumeBuy" style={{ width: "15vw", height: "70vh", marginLeft: "1px" }}>
+                        <div className="sticky-top resumeBuy1" style={{ padding: "5px", marginTop: "10vh" }}>
+                            <div className="border-bottom border-2">
 
-                                    </Row>
-                                    <Row style={{ display: "flex", justifyContent: "center" }}>
-                                        <p className="fs-4">Fecha de entrega {pedido.f_requerida.substring(0, 10)}</p>
-                                    </Row>
-                                    <Row style={{ display: "flex", justifyContent: "center" }}>
-                                        <p className="fs-6">La compra fue realizada el {pedido.f_pedido.substring(0, 10)}</p>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                    <Row className="mt-3">
-                        <p><b>Productos:</b></p>
-                    </Row>
-                    {
-                        pedido.ItemsPedidos.map((i, el) => (
-                            <Card className="mb-3">
-                                <Card.Header as="h5">{i.nombre}</Card.Header>
-                                <Card.Body>
-                                    <Row className="row row-wrap">
-                                        <Col className="col col-lg-10">
-                                            <Card.Title>Peso: {i.peso} kg</Card.Title>
-                                            <Card.Text>
-                                                <p>Cantidad: {i.cantidad}</p>
-                                                <p>Tipo de corte: {i.tipo_corte}</p>
-                                            </Card.Text>
-                                        </Col>
-                                        <Col className="col col-lg-2 col-md-2 position-relative">
-                                            <img src={i.Producto.fotos[0]} alt={i.nombre} className="rounded-circle img-fluid position-absolute" style={{width:"15vh", top: "1rem", right: "1.25rem"}}></img>
-                                        </Col>
-
-                                    </Row>
-                                    <Button variant="primary">Opinar sobre el producto</Button>
-                                </Card.Body>
-                            </Card>
-                        ))
-                    }
-                </div>
-                <div className="col col-sm-2 sticky-top resumeBuy" style={{ width: "15vw", height: "70vh", marginLeft: "1px" }}>
-                    <div className="sticky-top resumeBuy1" style={{ padding: "5px", marginTop: "10vh" }}>
-                        <div className="border-bottom border-2">
-
-                            <p className="fw-bolder fs-5 mb-0" >
-                                Detalle de compra
-                            </p>
-                            <p className="fw-light">
-                                {pedido.f_pedido.substring(0, 10)} # {pedido.pago_id}
-                            </p>
-                        </div>
-                        <div className="d-flex justify-content-between border-bottom mt-4">
-                            <p className="mb-0">
-                                Total en productos:
-                            </p>
-                            <p> ${pedido.ItemsPedidos.reduce((i, p) => i += p.precioTotal, 0)}</p>
-                        </div>
-                        <div className="d-flex justify-content-between border-bottom mt-2">
-                            <p className="mb-0">
-                                Envio:
-                            </p>
-                            <p>
-                                $3220
-                            </p>
-                        </div>
-                        <div className="d-flex justify-content-between mt-2">
-                            <p className="mb-0">
-                                Total:
-                            </p>
-                        <p>
-                            ${pedido.ItemsPedidos.reduce((i, p) => i += p.precioTotal, 0) + 3220}
-                        </p>
+                                <p className="fw-bolder fs-5 mb-0" >
+                                    Detalle de compra
+                                </p>
+                                <p className="fw-light">
+                                    {pedido.f_pedido.substring(0, 10)} # {pedido.pago_id}
+                                </p>
+                            </div>
+                            <div className="d-flex justify-content-between border-bottom mt-4">
+                                <p className="mb-0">
+                                    Total en productos:
+                                </p>
+                                <p> ${pedido.ItemsPedidos.reduce((i, p) => i += p.precioTotal, 0)}</p>
+                            </div>
+                            <div className="d-flex justify-content-between border-bottom mt-2">
+                                <p className="mb-0">
+                                    Envio:
+                                </p>
+                                <p>
+                                    $3220
+                                </p>
+                            </div>
+                            <div className="d-flex justify-content-between mt-2">
+                                <p className="mb-0">
+                                    Total:
+                                </p>
+                                <p>
+                                    ${pedido.ItemsPedidos.reduce((i, p) => i += p.precioTotal, 0) + 3220}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
     );
 }
