d3a1517a872bc104372978d68c2580d01f91e94c
Franco <fegll87@gmail.com>
2022-04-05 23:22:52 -0300

    login, register and footer styles

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -152,8 +152,8 @@ export const LoginUser = ({ setAuth }) => {
   }
 
   return (
-    <div className="bg-dark" style={{ height: "70vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundSize: "cover", backgroundImage: "url(https://estaticos.muyinteresante.es/uploads/images/article/5a37f7435cafe848e93c9869/carne-roja_0.jpg)" }}>
-      <div className='alert align-middle bg-dark text-light bg-opacity-75 mb-0 mx-3' style={{ display: "flex", height: "450px", flexDirection: "column", justifyContent: "center", minWidth: "300px", maxWidth: "300px" }}>
+    <div className="bg-dark" style={{ minHeight: "70vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundSize: "cover", backgroundImage: "url(https://estaticos.muyinteresante.es/uploads/images/article/5a37f7435cafe848e93c9869/carne-roja_0.jpg)" }}>
+      <div className='alert align-middle bg-dark text-light bg-opacity-75 mt-3' style={{ display: "flex", flexDirection: "column", justifyContent: "center", minWidth: "300px", maxWidth: "300px" }}>
         <h1 className='text-center mb-3'>Ingresar</h1>
         <div className=" d-flex justify-content-center">
           <div className='' style={{ width: "100%" }}>
@@ -184,23 +184,36 @@ export const LoginUser = ({ setAuth }) => {
               </div>
               <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label">Contraseña</label>
-                  <div style={{position:"relative"}}>
-                   <input type="password" name="contraseña" style={{borderRadius:"5px", height:"40px", width:"265px"}} id="show" aria-describedby="emailHelp" value={contraseña} placeholder="contraseña..."  onChange={e => onChange(e)} />
-                   <span style={{position:"absolute", right:"10px", cursor:"pointer", top:"50%", transform:"translateY(-50%)", color:"grey"}} className="icon-eye" onClick={showPassword} id="eye">
-                     <i class="fa-solid fa-eye-slash" id="icon"></i>
-                   </span>               
-                 </div>
+                <div style={{position:"relative"}}>
+                  <input 
+                    type="password" 
+                    name="contraseña" 
+                    id="show" 
+                    aria-describedby="emailHelp" 
+                    value={contraseña} 
+                    placeholder="contraseña..."  
+                    onChange={e => onChange(e)} 
+                    className='form-control'
+                  />
+                  <span 
+                    style={{position:"absolute", right:"10px", cursor:"pointer", top:"50%", transform:"translateY(-50%)", color:"grey"}} 
+                    className="icon-eye" 
+                    onClick={showPassword} 
+                    id="eye"
+                  >
+                    <i class="fa-solid fa-eye-slash" id="icon"></i>
+                  </span>               
+                </div>
               </div>
             </form>
             <div className="mb-3">
-              <button onClick={onSubmitForm} className="btn btn-success btn-block" disabled={inputs.correo === "" || inputs.contraseña === ""}>Submit</button>
+              <button onClick={onSubmitForm} className="btn btn-success btn-block w-100" disabled={inputs.correo === "" || inputs.contraseña === ""}>Submit</button>
             </div>
             <div className='d-flex justify-content-center align-items-center'>
-
               <h6 className='fs-5 align-self-center'>¿No estás registrado?</h6><Link className="fs-5 mb-2 ms-2 text-decoration-none fw-normal" to="/register">Registrate</Link>
             </div>
             <div className='d-flex justify-content-center align-items-center'>
-            <h6 className='fs-5 align-self-center'></h6><Link className="fs-5 mb-2 ms-2 text-decoration-none fw-normal" to="/login/forgot">¿Olvidaste la contraseña?</Link>
+              <h6 className='fs-5 align-self-center'></h6><Link className="fs-5 mb-2 ms-2 text-decoration-none fw-normal" to="/login/forgot">¿Olvidaste la contraseña?</Link>
             </div>
           </div>
         </div>
