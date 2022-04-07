ddd7bc84b6f7c52df469e7ea7b8700462be4548f
unknown <apoc88@protonmail.com>
2022-04-07 11:43:43 -0500

    uyqwwqe

--- src/Pages/Admin/Views/UserList.jsx
+++ src/Pages/Admin/Views/UserList.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { RiArrowLeftSLine } from 'react-icons/ri';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import swal from "sweetalert";
 import { getUsers, resetAdmin } from '../../../actions';
-import swal from "sweetalert"
 
 
 export default function UserList() {
@@ -17,17 +17,19 @@ export default function UserList() {
   }, []);
 
   const handleRedirect = (id) => {
-    navigate(`/user/${id}`)
-  }
+    navigate(`/user/${id}`);
+  };
 
   const handleReset = (correo) => {
     swal({
       text: "password reset exitoso",
       icon: "success",
       timer: "2000",
-    })
-    dispatch(resetAdmin({correo: correo}))
-  }
+    });
+    dispatch(resetAdmin({ correo: correo }));
+  };
+
+  console.log(users);
 
   return (
     <div className='container'>
@@ -35,6 +37,7 @@ export default function UserList() {
         <table className="table text-nowrap">
           <thead  >
             <tr className="text-sm">
+              <th className="border-gray-300 border-top py-3">Activo</th>
               <th className="border-gray-300 border-top py-3">Correo</th>
               <th className="border-gray-300 border-top py-3">Nombres</th>
               <th className="border-gray-300 border-top py-3">Telefono</th>
@@ -46,13 +49,14 @@ export default function UserList() {
               users.map(user => {
                 return (
                   <tr className="ltext-sm mt-3" key={user.correo}>
+                    <td>{String(user.activo)}</td>
                     <td>{user.correo}</td>
                     <td>{user.nombre} {user.apellido}</td>
                     <td>{user.celular}</td>
                     <td>{user.direccion}</td>
                     <td>
-                      <button value={user.correo} className="btn btn-success text-light text-decoration-none fs-6 mx-2" onClick={()=>handleRedirect(user.correo)}>Editar</button>
-                      <button value={user.correo} className="btn btn-success text-light text-decoration-none fs-6 mx-2" onClick={()=>handleReset(user.correo)}>Reset</button>
+                      <button value={user.correo} className="btn btn-success text-light text-decoration-none fs-6 mx-2" onClick={() => handleRedirect(user.correo)}>Editar</button>
+                      <button value={user.correo} className="btn btn-success text-light text-decoration-none fs-6 mx-2" onClick={() => handleReset(user.correo)}>Reset</button>
                     </td>
                   </tr>
                 );
